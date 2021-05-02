303151d14b759413d1cb747d408960d3af909b29
BOUGE Yoann <yoann.bouge@etu.univ-lyon1.fr>
2021-05-02 14:31:48 +0200

    Add commments in anomaliesDetector.py

--- anomaliesDetector.py
+++ anomaliesDetector.py
@@ -1,5 +1,27 @@
+# Skeleton extractor : 
+# INPUT : 1 sentence 
+# OUTPUT : Skeleton of this sentence
+
+# DistanceCalculator : 
+# INPUT : 2 sentences
+# OUTPUT : Distance
+
+# Class cluster : 
+    # Skeleton (ex: TCP Job name)
+    # DeleteElement (ex: tab[indices des mots])
+
+## LEARNING ##
+
+# Clusteriser : Use Distance function
+# INPUT : Many Skeletons
+# OUPUT : Clusters of Skeletons 
 
 
 
 if __name__ == "__main__":
     # Main
+
+
+    #GetCLuster
+    #GetSentence
+    #If (distance(newSentence, Cluster.skeleton) < seuil) :
