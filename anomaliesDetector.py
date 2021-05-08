b3bd9a565a3f38a78e8dbdaa60f3c21bfe23c71f
GG <wh.ggarcia@gmx.fr>
2021-05-08 12:28:26 +0200

    Added tools and corrected calculate_distance

--- anomaliesDetector.py
+++ anomaliesDetector.py
@@ -1,6 +1,15 @@
 import re
+import math
 
-## TOOL ##
+######### GLOBAL ##########
+
+mu = -1
+
+##################### TOOL ################
+
+def print_matrix(matrics):
+    for i in range(len(matrics)):
+        print(matrics[i])
 
 #Removes empty items in list
 # INPUT : List
@@ -34,6 +43,11 @@ def calculate_distance(phrase1, phrase2):
     l1 = len(phrase1)
     l2 = len(phrase2)
 
+    if(l1==0 or l2 == 0):
+        return max(l1, l2)
+
+    l1 += 1
+    l2 += 1
     d = [[0 for x in range(l2)] for i in range(l1)]
     substitutionCost = 0;
     for i in range (l1):
@@ -43,12 +57,14 @@ def calculate_distance(phrase1, phrase2):
     
     for i in range (1, l1):
         for j  in range(1, l2):
-            if(phrase1[i] == phrase2[j]):
+            if(phrase1[i-1] == phrase2[j-1]):
                 substitutionCost = 0
             else:
                 substitutionCost = 1
             d[i][j] = min(d[i-1][j] + 1, d[i][j-1] + 1, d[i-1][j-1]+substitutionCost)
-    return d[l1-1][l2-1]
+    print_matrix(d)
+    indexes = get_indexes(d)
+    return [d[l1-1][l2-1], indexes]
 
 # Class cluster : 
     # Skeleton (ex: TCP Job name)
@@ -64,11 +80,21 @@ def calculate_distance(phrase1, phrase2):
 
 if __name__ == "__main__":
     # Main
-    phrase1 = "[173.23.43.183] TCP (JOb Name Something) at /home/yolo/argh and many more"
-    phrase2 = "[173.23.43.183] TCP (Job Name OtherThing) at /home/yolo/oof and many others more"
+    phrase1 = "I ate a cat today"
+    phrase2 = "I ate a cat yesterday"
+    '''
+    phrase1 = "[173.23.43.183] TCP (Job Name Something) at /home/yolo/argh and many more"
+    phrase2 = "[173.23.43.183] TCP (Job Name OtherThing) at /home/yolo/oof and more many"
+    '''
+    print(calculate_WED(phrase1, phrase2))
+
+
+    '''
     liste1 = (remove_empty(extract_skeleton(phrase1).split(" ")))
     liste2 = (remove_empty(extract_skeleton(phrase2).split(" ")))
+    print(liste1, liste2)
     print(calculate_distance(liste1, liste2))
+    '''
 
     #GetCLuster
     #GetSentence
