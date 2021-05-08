45726b8073cc05893a28a510a6b9be69e90b83e7
GG <wh.ggarcia@gmx.fr>
2021-05-08 10:54:18 +0200

    Added first distance calculator

--- anomaliesDetector.py
+++ anomaliesDetector.py
@@ -28,6 +28,25 @@ def extract_skeleton(phrase):
 # DistanceCalculator (levenshtein distance): 
 # INPUT : 2 sentences skeletons (strings)
 # OUTPUT : Distance
+def calculate_distance(phrase1, phrase2):
+    l1 = len(phrase1)
+    l2 = len(phrase2)
+
+    d = [[0 for x in range(l2)] for i in range(l1)]
+    substitutionCost = 0;
+    for i in range (l1):
+        d[i][0] = i
+    for j in range (l2):
+        d[0][j] = j
+    
+    for i in range (1, l1):
+        for j  in range(1, l2):
+            if(phrase1[i] == phrase2[j]):
+                substitutionCost = 0
+            else:
+                substitutionCost = 1
+            d[i][j] = min(d[i-1][j] + 1, d[i][j-1] + 1, d[i-1][j-1]+substitutionCost)
+    return d[l1-1][l2-1]
 
 # Class cluster : 
     # Skeleton (ex: TCP Job name)
@@ -43,8 +62,11 @@ def extract_skeleton(phrase):
 
 if __name__ == "__main__":
     # Main
-    phrase = "[173.23.43.183] TCP (JOb Name MesCouilles) at /home/yolo/argh and many more"
-    print(extract_skeleton(phrase))
+    phrase1 = "[173.23.43.183] TCP (JOb Name Something) at /home/yolo/argh and many more"
+    phrase2 = "[173.23.43.183] TCP (Job Name OtherThing) at /home/yolo/oof and many others more"
+    liste1 = (remove_empty(extract_skeleton(phrase1).split(" ")))
+    liste2 = (remove_empty(extract_skeleton(phrase2).split(" ")))
+    print(calculate_distance(liste1, liste2))
 
     #GetCLuster
     #GetSentence
